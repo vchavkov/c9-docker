c4b17b2b8474c443bbcfce609d2dae6179529ffe
vchavkov <vlado@chavkov.com>
2018-02-18 10:05:43 +0200

    supervisor clean up.

--- cloud9/conf/launch.sh
+++ cloud9/conf/launch.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-/home/vchavkov/.nvm/versions/node/v4.8.7/bin/node /home/vchavkov/.cloud9/server.js -l 0.0.0.0 -p 8185 -w /ghost -a vchavkov:vchavkov
+
+if [ "$1" = "" || "$2" = "" ]; then
+	echo "cloud9 username and password are required."
+    exit 11;
+fi
+
+C9_USER=$1;
+C9_PASS=$2;
+
+su -c "/home/vchavkov/.nvm/versions/node/v4.8.7/bin/node /home/vchavkov/.cloud9/server.js -s standalone --server-config standalone --server-settings standalone --listen 0.0.0.0 --port 80 -w /ghost -a ${C9_USER}:${C9_PASS}" - vchavkov
