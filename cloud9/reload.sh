5c3be6dd092b42d907812470b414f6c63f3d0cf1
Vladimir Chavkov <vchavkov@assistance.bg>
2018-02-19 13:56:40 +0200

    + docker container reload script.

---
+++ cloud9/reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+	echo "Docker image id is required."
+    exit 11;
+fi
+
+DOCKER_IMAGE_ID=${1};
+
+# create cloud9 container
+docker run -it -d --restart=always -p 40000:80 --name vchavkov -e C9_USER=vchavkov -e C9_PASS=aiTh3oop -v /ghost:/workspace ${DOCKER_IMAGE_ID}
+docker run -it -d --restart=always -p 31000:80 --name ndemirev -e C9_USER=ndemirev -e C9_PASS=jo2eeLah -v /ghost/customer:/workspace ${DOCKER_IMAGE_ID}
+docker run -it -d --restart=always -p 31001:80 --name asaraliyski -e C9_USER=asaraliyski -e C9_PASS=Eijo6eek -v /ghost/customer/asaraliyski:/workspace ${DOCKER_IMAGE_ID}
