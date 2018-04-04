cc3c44f37ae1bd11c6f3f39c6dcda15c3bc6791f
Vladimir Chavkov <vlado@chavkov.com>
2018-04-04 10:55:53 +0300

    Added trim spaces utilities.

--- cloud9/conf/etc/profile.d/ghost.sh
+++ cloud9/conf/etc/profile.d/ghost.sh
@@ -4,6 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-if [ -d /ghost/.bin ]; then
-    PATH="/ghost/.bin:$PATH"
+if [ -d /workspace/.bin ]; then
+    PATH="/workspace/.bin:$PATH"
 fi
