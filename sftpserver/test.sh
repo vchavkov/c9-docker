2f4e6f417afdc01bd852a3140f4db88d8c98d55d
vchavkov <vchavkov@assistance.bg>
2018-10-09 16:41:49 +0300

    + sftpserver

--- sftpserver/test.sh
+++ sftpserver/test.sh
@@ -2,12 +2,12 @@
 
 docker rm -f $(docker ps -a -q);
 
-# DOCKER_IMAGE="$(docker build . --no-cache | grep "Successfully built" | cut -d " " -f3)"
-DOCKER_IMAGE="$(docker build . | grep "Successfully built" | cut -d " " -f3)"
+DOCKER_IMAGE="$(docker build . --no-cache | grep "Successfully built" | cut -d " " -f3)"
 
 docker run -it \
   --name sftp \
   -p 22222:22222 \
-  -v sftp-data:/data \
+  -v /tmp/data:/data \
   ${DOCKER_IMAGE} \
-  /bin/sh -c "/opt/sftpd/bin/sftpd.sh run"
+#   /bin/sh -c "/opt/sftpd/bin/sftpd.sh run"
+  /bin/sh
