2e443b328bab4e401a80ec573686d0ab955b088f
vchavkov <vchavkov@assistance.bg>
2018-11-20 16:13:15 +0200

    + docker nginx cache purge.

---
+++ nginx-cache-purge/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NGINX_CACHE_DIR="/tmp/nginx/cache"
+mkdir -p ${NGINX_CACHE_DIR}
+
+docker rm -f $(docker ps -a -q);
+
+DOCKER_IMAGE="$(docker build . --no-cache | grep "Successfully built" | cut -d " " -f3)"
+
+docker run -it \
+  --name nginx \
+  -p 88:80 \
+  -p 8443:443 \
+  -v $NGINX_CACHE_DIR:/var/cache/nginx \
+  ${DOCKER_IMAGE} \
+  /bin/bash
