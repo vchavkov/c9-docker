6a88b08d4bf23634bb71cbd2794cf87d5c50ae97
vchavkov <vchavkov@assistance.bg>
2018-11-21 10:36:00 +0200

    + docker nginx cache purge.

--- nginx-cache-purge/build.sh
+++ nginx-cache-purge/build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+URI="proxy.minerva.net"
+
 NGINX_CACHE_DIR="/tmp/nginx/cache"
 mkdir -p ${NGINX_CACHE_DIR}
 
 docker rm -f $(docker ps -a -q);
 
-# DOCKER_IMAGE="$(docker build . --no-cache | grep "Successfully built" | cut -d " " -f3)"
+# DOCKER_IMAGE="$(docker build --build-arg URI=${URI} . --no-cache | grep "Successfully built" | cut -d " " -f3)"
 
-DOCKER_IMAGE="$(docker build . | grep "Successfully built" | cut -d " " -f3)"
+DOCKER_IMAGE="$(docker build --build-arg URI=${URI} . | grep "Successfully built" | cut -d " " -f3)"
 
-VHOST=proxy.minerva.net \
-DOCKER_IMAGE=${DOCKER_IMAGE} \
-docker run -it \
+docker run \
+-it \
 --name nginx \
+--hostname=nginx \
+--dns=172.31.6.89 \
+--dns-search=minerva.net \
+--add-host "${URI}":127.0.0.1 \
 -p 88:80 \
 -p 8443:443 \
 -v $NGINX_CACHE_DIR:/var/cache/nginx \
