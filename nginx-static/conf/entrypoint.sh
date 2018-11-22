096b6f1d6862a4f9421b5329edee5e0936b264bf
vchavkov <vchavkov@assistance.bg>
2018-11-22 19:23:25 +0200

    + dnsmasq

--- nginx-static/conf/entrypoint.sh
+++ nginx-static/conf/entrypoint.sh
@@ -3,5 +3,13 @@
 set -o errexit
 set -o pipefail
 
-echo "===> Start supervisor"
+echo -e "\n===> updated /etc/resolv.conf\n"
+echo "
+nameserver 127.0.0.1
+search minerva.net
+" > /etc/resolv.conf
+
+cat /etc/resolv.conf
+
+echo -e "===> start supervisor\n"
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
