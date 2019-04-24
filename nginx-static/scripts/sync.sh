b5b27c2e92c971dad6837beab39bbabf39e85764
vchavkov <vchavkov@assistance.bg>
2019-04-24 18:20:33 +0300

    progress on nginx

--- nginx-static/scripts/sync.sh
+++ nginx-static/scripts/sync.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-rsync -av --delete --stats ./ --exclude="sync.sh" root@192.168.3.114:nginx/
-rsync -av --delete --stats ./ --exclude="sync.sh" root@192.168.3.114:/etc/nginx/site-enabled/
+rsync -av --delete ./ --exclude="sync.sh" root@192.168.3.114:nginx/
+
+rsync -av --delete ../conf/etc/nginx/sites-enabled/ root@192.168.3.114:/etc/nginx/sites-enabled/
+rsync -av --delete ../conf/etc/nginx/conf.d/ root@192.168.3.114:/etc/nginx/conf.d/
+rsync -av --delete ../conf/etc/nginx/snippets/ root@192.168.3.114:/etc/nginx/snippets/
+rsync -av ../conf/etc/nginx/nginx.conf root@192.168.3.114:/etc/nginx/nginx.conf
